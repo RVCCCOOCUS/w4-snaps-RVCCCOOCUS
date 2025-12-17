e8e93722eead1ce79a3ba62657482ddf840d895a
RVCCCOOCUS <cgar6999@stu.raritanval.edu>
2025-12-16 23:01:11 -0500

    final build

---
+++ Assets/start_game.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class start_game : MonoBehaviour
+{
+    public void StartTheGame()
+    {
+        SceneManager.LoadScene("midterm project");
+    }
+}
